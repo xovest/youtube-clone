9fb912d9ebda1d9c984b6ae3d9e954fb376cb7b7
xovest <79008917+xovest@users.noreply.github.com>
2021-12-11 09:45:51 +0200

    video and src details

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import youtube from './api/youtube'
 import { SearchBar, VideoDetail } from './components/'
 
 class App extends React.Component {
+  handleSubmit = async (searchTerm) => {
+    const res = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: process.env.API_KEY,
+        q: searchTerm,
+      }
+    })
+
+    console.log(res);
+  }
+
   render() {
     return (
       <Grid justifyContent="center" container spacing={16}>
         <Grid item xs={12}>
           <Grid container spacing={16}>
             <Grid item xs={12}>
-              <SearchBar />
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={8}>
               <VideoDetail />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,30 @@
+import { Paper, TextField } from '@mui/material'
 import React from 'react'
 
 class SearchBar extends React.Component {
+  //we could also use useState, same shit
+  state = {
+    searchTerm: '',
+  }
+
+  handleChange = (e) => this.setState({ searchTerm: e.target.value })
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { searchTerm } = this.state;
+    const { onFormSubmit } = this.props;
+
+    onFormSubmit(searchTerm)
+  }
+
   render() {
     return (
-      <h1>srcbar</h1>
+      <Paper elevation={6} style={{ padding: '25px' }}>
+        <form onSubmit={this.handleSubmit}>
+          <TextField fullWidth label="Search..." onChange={this.handleChange} />
+        </form>
+      </Paper>
     )
   }
 }
