31c0ee70c88062eff054a3bb35083030c0797fe4
xovest <79008917+xovest@users.noreply.github.com>
2021-12-11 09:45:02 +0200

    api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+
+import youtube from './api/youtube'
 
 class App extends React.Component {
   render () {
     return (
-      <h1>yo</h1>
+      <Grid justifyContent="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* src bar */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* video details */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* video list */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     )
   }
 }
